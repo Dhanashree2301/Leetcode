faab92c1abffbd6df6007c4f0d45ac3c456f2fff
Dhanashree Patil <143509479+Dhanashree2301@users.noreply.github.com>
2023-09-20 20:37:47 +0530

    Time: 19 ms (58.94%), Space: 13.1 MB (88.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,9 +1,17 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        for i in range(n):
+        #FIRST SOLUTION
+        """for i in range(n):
             nums1.pop()
         nums1+=nums2
         print(nums1.sort())
+        
+        
+        #SECOND SOLUTION
+        """
+        for i in range(n):
+            nums1[m+i]=nums2[i]
+        nums1.sort()
         """
         :type nums1: List[int]
         :type m: int
