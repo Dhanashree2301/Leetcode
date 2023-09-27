65539de7110ea55a5dbabcb50de147a6516556e3
Dhanashree Patil <143509479+Dhanashree2301@users.noreply.github.com>
2023-09-27 20:45:07 +0530

    Time: 10 ms (84.69%), Space: 54.8 MB (82.12%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // boolean result=false;
-        // for(int i=0;i<nums.length-1;i++)
-        // {
-        //     for(int j=i+1;j<nums.length;j++)
-        //     {
-        //         if(nums[i]==nums[j])
-        //         {
-        //             result=true;
-        //         }
-        //     }
-        // }
-        // return result;
-        
          HashSet<Integer> hset=new HashSet();
          for(int ind=0;ind<nums.length;ind++)
          {
@@ -26,17 +13,3 @@ class Solution {
         return false;
     }
 }
-
-
-//  HashSet<Integer> hset = new HashSet<Integer>();
-//         // Traverse all the elements through the loop...
-//         for (int idx = 0; idx < nums.length; idx ++){
-//             // Searches hashset. if present, it contains duplicate...
-//             if (hset.contains(nums[idx])){
-//                 return true;
-//             }
-//             // if not present it will update hashset...
-//             hset.add(nums[idx]);
-//         }
-//         // Otherwise return false...
-//         return false;
